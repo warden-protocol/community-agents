b4911d0947f4faf36f044bb06c424906b5717ab5
Ivan Sanin <ivan.sanin@eqlab.online>
2025-11-01 10:49:03 +0300

    add market changes and top gainers, losers

--- packages/portfolio-agent/src/utils/coingecko.ts
+++ packages/portfolio-agent/src/utils/coingecko.ts
@@ -1,5 +1,9 @@
 import Coingecko from '@coingecko/coingecko-typescript';
 import { ListGetResponse } from '@coingecko/coingecko-typescript/resources/coins/list.mjs';
+import { TopToken } from './portfolio';
+import { TopGainersLoserGetResponse } from '@coingecko/coingecko-typescript/resources/coins/top-gainers-losers.mjs';
+
+export type Duration = '24h' | '7d' | '30d';
 
 type CoingeckoToken = ListGetResponse.ListGetResponseItem & {
   decimals: number | undefined;
@@ -7,7 +11,8 @@ type CoingeckoToken = ListGetResponse.ListGetResponseItem & {
 type CoingeckoTokenMetadata = CoingeckoToken & {
   decimals: number | undefined;
 };
-type PriceChange = {
+export type PriceChange = {
+  id: string;
   firstPriceUsd: number;
   lastPriceUsd: number;
   priceChangeUsd: number;
@@ -115,10 +120,71 @@ export class CoingeckoService {
       firstPriceUsd !== 0 ? (priceChangeUsd / firstPriceUsd) * 100 : 0;
 
     return {
+      id: coingeckoId,
       firstPriceUsd,
       lastPriceUsd,
       priceChangeUsd,
       priceChangePercent,
+    };
+  }
+
+  async getMarketChanges(
+    ids: string[],
+    priceChange: Duration,
+  ): Promise<PriceChange[]> {
+    const markets = await this.coingeckoClient.coins.markets.get({
+      ids: ids.join(','),
+      vs_currency: 'usd',
+      price_change_percentage: priceChange,
+      per_page: 250,
+      precision: '10',
+    });
+
+    //Object key example: price_change_percentage_30d_in_currency
+    const priceChangeKey = `price_change_percentage_${priceChange}_in_currency`;
+    return markets.map((m) => {
+      const priceChangePercentage = Number(m[priceChangeKey]);
+      const startPeriodPrice =
+        m.current_price / (priceChangePercentage / 100.0 + 1.0);
+      return {
+        id: m.id,
+        firstPriceUsd: startPeriodPrice,
+        lastPriceUsd: m.current_price,
+        priceChangeUsd: m.current_price - startPeriodPrice,
+        priceChangePercent: priceChangePercentage,
+      };
+    });
+  }
+
+  async getTopGainersLosers(
+    duration: Duration,
+  ): Promise<{ topGainers: TopToken[]; topLosers: TopToken[] }> {
+    const response = await this.coingeckoClient.coins.topGainersLosers.get({
+      vs_currency: 'usd',
+      duration,
+      top_coins: '300',
+    });
+
+    const convertToTopToken = (
+      coin:
+        | TopGainersLoserGetResponse.TopGainer
+        | TopGainersLoserGetResponse.TopLoser,
+    ): TopToken => {
+      const priceChangeKey = `usd_${duration}_change`;
+      const priceChange = Number(coin[priceChangeKey]);
+      return {
+        coingeckoId: coin.id,
+        symbol: coin.symbol,
+        name: coin.name,
+        marketCapRank: coin.market_cap_rank,
+        currentPrice: coin.usd,
+        priceChange,
+      };
+    };
+
+    return {
+      topGainers: response.top_gainers.map(convertToTopToken),
+      topLosers: response.top_losers.map(convertToTopToken),
     };
   }
 }
